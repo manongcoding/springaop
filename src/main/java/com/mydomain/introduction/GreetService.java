4aef2c7be400b8d58149b5e5de201ea69e1c2684
manongcoding <2523763996@qq.com>
2018-09-07 16:50:56 +0800

    d

---
+++ src/main/java/com/mydomain/introduction/GreetService.java
@@ -0,0 +1,5 @@
+package com.mydomain.introduction;
+
+public interface GreetService {
+    void greeting(String name);
+}
--- src/main/java/com/mydomain/introduction/Main.java
+++ src/main/java/com/mydomain/introduction/Main.java
@@ -5,12 +5,6 @@ import com.mydomain.manager.TaskManagerImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Main
- *
- * @author jyl25609
- * @version Id: Main, v 0.1 18-9-7 下午4:42 jyl25609 Exp $
- */
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("introductionContext.xml");
